911a3a89a0482d9a2e7f0566893f397a558f2eb3
BurlakovIvan <burlakovid@inbox.ru>
2022-06-02 20:19:53 +0300

    2.1. Рефакторинг - Расстояние между точками. [#122587]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -1,19 +1,36 @@
 package ru.job4j.condition;
 
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
 public class Point {
-    public static double distance(int x1, int y1, int x2, int y2) {
-        double rsl = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-        return rsl;
+    private int x;
+    private int y;
+
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+
+    public double distance(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
     }
 
     public static void main(String[] args) {
-        double result = Point.distance(0, 0, 2, 0);
+        Point pointFirst = new Point(0, 0);
+        Point pointSecond = new Point(2, 0);
+        double result = pointFirst.distance(pointSecond);
         System.out.println("result (0, 0) to (2, 0) " + result);
-        result = Point.distance(0, 0, 15, 8);
+        pointFirst = new Point(0, 0);
+        pointSecond = new Point(15, 8);
+        result = pointFirst.distance(pointSecond);
         System.out.println("result (0, 0) to (15, 8) " + result);
-        result = Point.distance(1, 8, 20, 4);
+        pointFirst = new Point(1, 8);
+        pointSecond = new Point(20, 4);
+        result = pointFirst.distance(pointSecond);
         System.out.println("result (1, 8) to (20, 4) " + result);
-        result = Point.distance(10, 80, 200, 45);
+        pointFirst = new Point(10, 80);
+        pointSecond = new Point(200, 45);
+        result = pointFirst.distance(pointSecond);
         System.out.println("result (10, 80) to (200, 45) " + result);
     }
 }
--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -8,44 +8,36 @@ public class PointTest {
     @Test
     public void when00to20then2() {
         double expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point pointFirst = new Point(0, 0);
+        Point pointSecond = new Point(2, 0);
+        double out = pointFirst.distance(pointSecond);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when00to158then17() {
         double expected = 17;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 15;
-        int y2 = 8;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point pointFirst = new Point(0, 0);
+        Point pointSecond = new Point(15, 8);
+        double out = pointFirst.distance(pointSecond);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when18to204then19dot416() {
         double expected = 19.416;
-        int x1 = 1;
-        int y1 = 8;
-        int x2 = 20;
-        int y2 = 4;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point pointFirst = new Point(1, 8);
+        Point pointSecond = new Point(20, 4);
+        double out = pointFirst.distance(pointSecond);
         Assert.assertEquals(expected, out, 0.01);
     }
 
     @Test
     public void when1080to20045then193dot196() {
         double expected = 193.196;
-        int x1 = 10;
-        int y1 = 80;
-        int x2 = 200;
-        int y2 = 45;
-        double out = Point.distance(x1, y1, x2, y2);
+        Point pointFirst = new Point(10, 80);
+        Point pointSecond = new Point(200, 45);
+        double out = pointFirst.distance(pointSecond);
         Assert.assertEquals(expected, out, 0.01);
     }
 }
